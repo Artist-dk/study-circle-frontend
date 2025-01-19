73d30907e49337aed1b89eacd30b7a896859d83e
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2025-01-19 18:42:26 +0530

    resetting project

--- backend/routes/settings.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const controller =  require('../controllers/settings')
-
-router.put('/theme',controller.theme);
-
-module.exports = router;
--- frontend/src/components/NoPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NoPage() {
-  return (
-    <div>
-      <h1>No Page</h1>
-    </div>
-  )
-}
